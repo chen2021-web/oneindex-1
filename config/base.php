f0ef9f98b62cda4de32e94311ebeb683cb7d5e65
chen2021-web <83762621+chen2021-web@users.noreply.github.com>
2021-07-15 14:16:20 +0800

    Update base.php

--- config/base.php
+++ config/base.php
@@ -1,6 +1,6 @@
 <?php return array (
   'client_id' => '0c8c471d-2e17-4efb-8d6f-c495553f0b56',
-  'client_secret' => '_i7rj-R~5j971W9~Fi7p.prkfoIUhpcIeX',
+  'client_secret' => 'zrRTK144{]fwfjqVVJC61!~',
   'redirect_uri' => 'https://ju.tn/onedrive-login',
   'dir_cache_time' => 600,
   'file_cache_time' => 86400,
